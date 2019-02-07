a8c42f46e8d5835f4b423ef352dc5f0444877937
gonggongjohn <gonggongjohn@163.com>
2019-02-07 13:39:29 +0800

    Establish Config System

--- src/main/java/com/gonggongjohn/eok/CommonProxy.java
+++ src/main/java/com/gonggongjohn/eok/CommonProxy.java
@@ -1,12 +1,14 @@
 package com.gonggongjohn.eok;
 
 import com.gonggongjohn.eok.handlers.BlockHandler;
+import com.gonggongjohn.eok.handlers.ConfigHandler;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
 public class CommonProxy{
     public void preInit(FMLPreInitializationEvent event){
+        new ConfigHandler(event);
     }
 
     public void init(FMLInitializationEvent event){
---
+++ src/main/java/com/gonggongjohn/eok/handlers/ConfigHandler.java
@@ -0,0 +1,34 @@
+package com.gonggongjohn.eok.handlers;
+
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.common.config.Configuration;
+import org.apache.logging.log4j.Logger;
+
+public class ConfigHandler {
+    private static Configuration config;
+    private static Logger logger;
+    public static int test;
+
+    public ConfigHandler(FMLPreInitializationEvent event){
+        logger = event.getModLog();
+        config = new Configuration(event.getSuggestedConfigurationFile());
+        config.load();
+        load();
+    }
+
+    public static void load(){
+        logger.info("Started loading config. ");
+        String comment;
+
+        comment = "A Test Configuration";
+        test = config.get(Configuration.CATEGORY_GENERAL, "test", 1, comment).getInt();
+
+        config.save();
+        logger.info("Finished loading config. ");
+    }
+
+    public static Logger logger()
+    {
+        return logger;
+    }
+}
