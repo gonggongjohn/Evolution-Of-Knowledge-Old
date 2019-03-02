c8cebf72da86ff0f1cd8d6f64b81a66b17c1d3ec
zijing <zijing@users.noreply.github.com>
2019-03-02 19:46:25 +0800

    优化代码结构
    进行了代码结构优化，添加了EventHandler类

---
+++ src/main/java/com/gonggongjohn/eok/handlers/EventHandler.java
@@ -0,0 +1,9 @@
+package com.gonggongjohn.eok.handlers;
+
+import com.gonggongjohn.eok.data.TreeTweaker;
+
+public class EventHandler {
+	public static void registerEvent() {
+		new TreeTweaker();
+	}
+}
