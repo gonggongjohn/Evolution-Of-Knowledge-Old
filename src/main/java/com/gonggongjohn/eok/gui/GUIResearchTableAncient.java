baf2d0558e9dd5488acc94bc4c0d036c439ebb1d
gonggongjohn <gonggongjohn@163.com>
2019-03-03 00:48:36 +0800

    New ResearchTable GUI

--- src/main/java/com/gonggongjohn/eok/gui/GUIResearchTableAncient.java
+++ src/main/java/com/gonggongjohn/eok/gui/GUIResearchTableAncient.java
@@ -4,65 +4,146 @@ import com.gonggongjohn.eok.EOK;
 import com.gonggongjohn.eok.containers.ContainerResearchTableAncient;
 import com.gonggongjohn.eok.tileEntities.TEResearchTableAncient;
 import com.gonggongjohn.eok.utils.ResearchUtils;
+import cpw.mods.fml.client.GuiScrollingList;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
 import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.resources.I18n;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.entity.player.InventoryPlayer;
 import net.minecraft.util.ResourceLocation;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.opengl.GL11;
 
-//原始研究台GUI界面（由于后续可能要放东西所以继承了GuiContainer）
+import java.util.Random;
+
 public class GUIResearchTableAncient extends GuiContainer{
-    //GUI贴图位置
-    private ResourceLocation texture = new ResourceLocation(EOK.MODID, "textures/gui/guiResearchTableAncient.png");
+    private ResourceLocation backTextureL = new ResourceLocation(EOK.MODID, "textures/gui/guiResearchTableAncientBackgroundLeft.png");
+    private ResourceLocation backTextureR = new ResourceLocation(EOK.MODID, "textures/gui/guiResearchTableAncientBackgroundRight.png");
+    private ResourceLocation componentTexture = new ResourceLocation(EOK.MODID, "textures/gui/guiResearchTableComponents.png");
+    private ResourceLocation paperTexture;
+    private int offsetX, offsetY;
     private InventoryPlayer inventory;
     private TEResearchTableAncient te;
     private static Logger logger;
+    private int screenPage;
+    private GuiScrollingList scrollingList;
 
-    //构造函数
     public GUIResearchTableAncient(TEResearchTableAncient te, EntityPlayer player) {
         super(new ContainerResearchTableAncient(te, player));
         inventory = player.inventory;
         this.te = te;
-        //GUI宽高（像素）
-        this.xSize = 255;
+        this.xSize = 360;
         this.ySize = 210;
         logger = LogManager.getLogger(EOK.MODID);
     }
 
-    //画背景层函数（实时刷新）
     @Override
     protected void drawGuiContainerBackgroundLayer(float par1, int par2, int par3) {
-        //加载贴图
-        Minecraft.getMinecraft().renderEngine.bindTexture(texture);
-        //画笔颜色校正
+        Minecraft.getMinecraft().renderEngine.bindTexture(backTextureL);
         GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-        //GUI在窗口中的位置
         int x = (width - xSize) / 2;
         int y = (height - ySize) / 2;
-        //画GUI背景（参数ctrl+左键点进去看）
-        drawTexturedModalRect(x, y, 0, 0, xSize, ySize);
+        drawTexturedModalRect(x, y, 56, 0, xSize / 2, ySize);
+        Minecraft.getMinecraft().renderEngine.bindTexture(backTextureR);
+        drawTexturedModalRect(x + xSize / 2, y, 20, 0, xSize / 2, ySize);
+        if(screenPage == 1){
+            scrollingList.drawScreen(par2, par3, par1);
+            Minecraft.getMinecraft().renderEngine.bindTexture(paperTexture);
+            drawTexturedModalRect(x + 170, y + 10, 0, 0, 180, 180);
+        }
+        //Minecraft.getMinecraft().renderEngine.bindTexture(componentTexture);
+        //drawTexturedModalRect(x + 160, y + 25, 0, 24, 8, 128);
     }
 
-    //画前景层函数（实时刷新）
     @Override
     protected void drawGuiContainerForegroundLayer(int mouseX,int mouseY){
 
     }
 
-    //GUI初始化（仅在打开时刷新一次）
     @Override
     public void initGui(){
         super.initGui();
-        //参考系计算（从以窗口为参考系变成以GUI背景贴图为参考系）
-        int offsetX = (this.width - this.xSize) / 2, offsetY = (this.height - this.ySize) / 2;
-        //添加任务按钮（参数ctrl+左键点进去看）
-        this.buttonList.add(new IRTAButton(2, offsetX + 20, offsetY + 25, 36, 36, "").setOffset(offsetX, offsetY, this.xSize, this.ySize));
-        for(int i = 4; i <= ResearchUtils.researchCount; i++) {
-        this.buttonList.add(new IRTAButton(i, offsetX + 20 + (i - 3) % 4 * 50, offsetY + 25  + (i - 2) / 5 * 46, 36, 36, "").setOffset(offsetX, offsetY, this.xSize, this.ySize));
+        Random rand = new Random();
+        paperTexture = new ResourceLocation(EOK.MODID, "textures/gui/paper_" + rand.nextInt(5) + ".png");
+        offsetX = (this.width - this.xSize) / 2;
+        offsetY = (this.height - this.ySize) / 2;
+        this.buttonList.add(new GuiButton(0, offsetX - 48, offsetY, 48, 16, ""){
+            @Override
+            public void drawButton(Minecraft mc, int mouseX, int mouseY){
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                Minecraft.getMinecraft().renderEngine.bindTexture(componentTexture);
+                this.drawTexturedModalRect(this.xPosition, this.yPosition, 16, 96, this.width, this.height);
+                String title = I18n.format("button.screenBrowse.title");
+                this.drawString(mc.fontRenderer, title, this.xPosition + (this.width - mc.fontRenderer.getStringWidth(title)) / 2, this.yPosition + 4, 0x404040);
+            }
+        });
+        this.buttonList.add(new GuiButton(1, offsetX - 48, offsetY + 24, 48, 16, "") {
+            @Override
+            public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                Minecraft.getMinecraft().renderEngine.bindTexture(componentTexture);
+                this.drawTexturedModalRect(this.xPosition, this.yPosition, 16, 96, this.width, this.height);
+                String title = I18n.format("button.screenResearch.title");
+                this.drawString(mc.fontRenderer, title, this.xPosition + (this.width - mc.fontRenderer.getStringWidth(title)) / 2, this.yPosition + 4, 0x404040);
+            }
+        });
+    }
+
+    @Override
+    protected void actionPerformed(GuiButton button){
+        if(button.id == 0){
+            screenPage = 0;
+            buttonList.remove(2);
         }
+        if(button.id == 1){
+            screenPage = 1;
+            initResearchScreen();
+        }
+    }
+
+    private void initResearchScreen(){
+        this.buttonList.add(new GuiButton(2, offsetX + 54, offsetY + 179, 64, 16, "") {
+            @Override
+            public void drawButton(Minecraft mc, int mouseX, int mouseY) {
+                GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
+                Minecraft.getMinecraft().renderEngine.bindTexture(componentTexture);
+                this.drawTexturedModalRect(this.xPosition, this.yPosition, 0, 8, this.width, this.height);
+                String title = I18n.format("button.startResearch.title");
+                this.drawString(mc.fontRenderer, title, this.xPosition + (this.width - mc.fontRenderer.getStringWidth(title)) / 2, this.yPosition + 4, 0x404040);
+            }
+        });
+        scrollingList = new GuiScrollingList(mc, 160, 10, offsetY + 18, offsetY + 170, offsetX + 14, 30) {
+            @Override
+            protected int getSize() {
+                return 20;
+            }
+
+            @Override
+            protected void elementClicked(int index, boolean doubleClick) {
+
+            }
+
+            @Override
+            protected boolean isSelected(int index) {
+                return false;
+            }
+
+            @Override
+            protected void drawBackground() {
+            }
+
+            @Override
+            protected void drawSlot(int index, int var2, int var3, int var4, Tessellator var5) {
+                Minecraft.getMinecraft().renderEngine.bindTexture(componentTexture);
+                drawTexturedModalRect(this.left + 20, var3, 16, 32, 22, 22);
+                //drawString(mc.fontRenderer, "" + index, this.left + 3, var3 + 2, 0x404040);
+            }
+        };
+        scrollingList.registerScrollButtons(buttonList, 3, 4);
     }
 }
 
