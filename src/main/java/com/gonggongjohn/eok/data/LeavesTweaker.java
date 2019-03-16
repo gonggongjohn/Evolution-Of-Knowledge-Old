d9df9f541ea0d126a5eb1028b7c61e5fb93ff6b8
zijing <zijing@users.noreply.github.com>
2019-03-16 17:14:29 +0800

    破坏树叶有70%几率掉落木棍
    破坏树叶有70%几率掉落木棍

---
+++ src/main/java/com/gonggongjohn/eok/data/LeavesTweaker.java
@@ -0,0 +1,31 @@
+package com.gonggongjohn.eok.data;
+
+import com.gonggongjohn.eok.handlers.ItemHandler;
+
+import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.world.BlockEvent;
+
+public class LeavesTweaker {
+	//作者:zi_jing
+	//用途:破坏树叶有70%几率掉落木棍
+
+		@SubscribeEvent
+		public void Tweak(BlockEvent.HarvestDropsEvent event) {
+			
+			//判断破坏的方块是否为树叶
+			if(event.block instanceof BlockLeaves) {
+				//添加掉落物
+				event.drops.add(new ItemStack(Items.stick));
+				event.dropChance = 0.7F;
+			}
+		}
+		
+		//注册事件
+		public LeavesTweaker() {
+			MinecraftForge.EVENT_BUS.register(this);
+		}
+}
--- src/main/java/com/gonggongjohn/eok/handlers/EventHandler.java
+++ src/main/java/com/gonggongjohn/eok/handlers/EventHandler.java
@@ -1,11 +1,13 @@
 package com.gonggongjohn.eok.handlers;
 
 import com.gonggongjohn.eok.data.GrassTweaker;
+import com.gonggongjohn.eok.data.LeavesTweaker;
 import com.gonggongjohn.eok.data.TreeTweaker;
 
 public class EventHandler {
 	public static void registerEvent() {
 		new TreeTweaker();
 		new GrassTweaker();
+		new LeavesTweaker();
 	}
 }
