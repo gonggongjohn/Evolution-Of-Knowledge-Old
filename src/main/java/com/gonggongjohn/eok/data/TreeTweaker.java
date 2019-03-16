ae0ec82718eb889efefb0fc34011e9ac7eb0fe9a
zijing <zijing@users.noreply.github.com>
2019-03-16 14:27:26 +0800

    Update TreeTweaker.java
    清除冗余代码并添加注释

--- src/main/java/com/gonggongjohn/eok/data/TreeTweaker.java
+++ src/main/java/com/gonggongjohn/eok/data/TreeTweaker.java
@@ -7,33 +7,44 @@ import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.player.PlayerEvent;
 
+
+//作者:zi_jing
+//用途:使玩家无法空手挖掘木头
+
 public class TreeTweaker {
 	@SubscribeEvent
 	public void Tweak(PlayerEvent.BreakSpeed event) {
+		//当玩家挖掘木头时执行
 		if(event.block instanceof BlockLog) {
+			//将玩家挖掘速度设为0
 			event.newSpeed = 0.0F;
+			//判断玩家手上是否持有物品,防止游戏崩溃
 			if(event.entityPlayer.inventory.getCurrentItem() != null) {
-				//System.out.println(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName());
-				if(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName().equals("item.hatchetWood")) {
+				//判断玩家是否持有斧头
+				switch(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName()) {
+				case "item.hatchetWood":
 					event.newSpeed = event.originalSpeed;
-				}
-				if(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName().equals("item.hatchetStone")) {
+					break;
+				case "item.hatchetStone":
 					event.newSpeed = event.originalSpeed;
-				}
-				if(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName().equals("item.hatchetIron")) {
+					break;
+				case "item.hatchetIron":
 					event.newSpeed = event.originalSpeed;
-				}
-				if(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName().equals("item.hatchetGold")) {
+					break;
+				case "item.hatchetGold":
 					event.newSpeed = event.originalSpeed;
-				}
-				if(event.entityPlayer.inventory.getCurrentItem().getUnlocalizedName().equals("item.hatchetDiamond")) {
+					break;
+				case "item.hatchetDiamond":
 					event.newSpeed = event.originalSpeed;
+					break;
+				default:
+					break;
 				}
 			}
 			
 		}
 	}
-	
+	//注册事件
 	public TreeTweaker() {
 		MinecraftForge.EVENT_BUS.register(this);
 	}
